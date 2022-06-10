7011f2c06a075f956dd927569e3dbfbc21730a8e
TheAndrii <towys505@gmail.com>
2022-06-10 14:10:02 +0300

    The final version

---
+++ app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(activeSlide = 3) {
+  const slides = document.querySelectorAll('.slide');
+
+  slides[activeSlide].classList.add('active');
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasess();
+
+      slide.classList.add('active');
+    });
+  }
+
+  function clearActiveClasess () {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+  }
+}
+
+slidesPlugin();
